1093c2b0b77be89bcfbd220c0ee04030f0d16c45
utnasun <utnasun2010@yandex.ru>
2025-01-06 20:49:55 +0300

    add deepspeech model

--- src/model/__init__.py
+++ src/model/__init__.py
@@ -1,5 +1,4 @@
 from src.model.baseline_model import BaselineModel
+from src.model.deepspeech_model import DeepSpeech
 
-__all__ = [
-    "BaselineModel",
-]
+__all__ = ["BaselineModel", "DeepSpeech"]
